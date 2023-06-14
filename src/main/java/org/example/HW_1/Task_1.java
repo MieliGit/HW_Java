294eff62ee9792fd2243144cc020f8d820930020
Zakharov Daniil <zdan14@mail.ru>
2023-06-14 13:32:45 +0300

    Небольшие изменения в задание один(Добавил метод) Плюс решение задания 2

--- src/main/java/org/example/HW_1/Task_1.java
+++ src/main/java/org/example/HW_1/Task_1.java
@@ -5,25 +5,31 @@ import java.util.Scanner;
 
 public class Task_1 {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); // Объявляем Scanner
+        Scanner input = new Scanner(System.in);
         System.out.println("Длина строки: ");
         int size = input.nextInt();
+
         String array[] = new String[size+1];
         System.out.println("Введите слова по очереди:");
         for (int i = 0; i <= size; i++) {
             array[i] = input.nextLine();
         }
+        task_1(size, array);
 
-        System.out.print ("Обычный вид:");
+    }
+
+    private static char task_1(int size, String[] array) {
+        System.out.print("Обычный вид:");
         for (int i = 0; i <= size; i++) {
-            System.out.print (" " + array[i]);
+            System.out.print(" " + array[i]);
         }
         System.out.println();
 
-        System.out.print ("Первернутый вид:");
+        System.out.print("Первернутый вид:");
         for (int i = size; i > 0; i--) {
-            System.out.print (" " + array[i]);
+            System.out.print(" " + array[i]);
         }
         System.out.println();
+        return 0;
     }
 }
--- src/main/java/org/example/HW_1/Task_2.java
+++ src/main/java/org/example/HW_1/Task_2.java
@@ -1,7 +1,36 @@
 package org.example.HW_1;
 
+import java.util.Scanner;
+
 public class Task_2 {
     public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Длина массива: ");
+        int size = input.nextInt();
+        int arr[] = new int[size];
+        System.out.println("Ввод элементов:");
 
+        for (int i = 0; i < size; i++) {
+            arr[i] = input.nextInt();
+        }
+        System.out.print ("Вывод:");
+        for (int i = 0; i < size; i++) {
+            System.out.print (" " + arr[i]);
+        }
+        System.out.println();
+        int sum = 0;
+
+        sumNumber(sum, size, arr);
+
+    }
+
+    private static int sumNumber(int sum, int size, int[] arr) {
+        for (int i = 0; i < size; i++) {
+            if (arr[i] < 0) {
+                sum += arr[i - 1];
+            }
+        }
+        System.out.println(sum);
+        return sum;
     }
 }
