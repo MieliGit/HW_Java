aa28e390b0d13252fd4d70db363e68234cd2b1e2
Zakharov Daniil <zdan14@mail.ru>
2023-07-02 22:57:18 +0300

    Добавление решения с произведением

--- src/main/java/org/example/HW_1_1/Task_1.java
+++ src/main/java/org/example/HW_1_1/Task_1.java
@@ -8,10 +8,13 @@ public class Task_1 {
         Scanner num  =  new Scanner(System.in);
         int n =  num.nextInt();
         int sum = 0;
+        int mul = 1;
 
-        for (int i = 0; i <= n; i++) {
+        for (int i = 1; i <= n; i++) {
             sum += i;
+            mul *= i;
         }
         System.out.println("n-ое треугольного число: " + sum);
+        System.out.println("Произведение числе от 1 до n: " + mul);
     }
 }
