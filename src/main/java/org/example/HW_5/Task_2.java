34ddf09b359ab426c03caf853d37f6b5d19be518
Zakharov Daniil <zdan14@mail.ru>
2023-07-04 13:06:18 +0300

    Решение 2 задачи,не доконца разобрал, как реализовать сортировку по величине значения при работе с Map, буду дорабатывать

---
+++ src/main/java/org/example/HW_5/Task_2.java
@@ -0,0 +1,38 @@
+package org.example.HW_5;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Task_2 {
+    public static void main(String[] args) {
+        String text = "Иван Иванов, Светлана Петрова, Кристина Белова, Анна Мусина, Анна Крутова, Иван Юрин, " +
+                "Петр Лыков, Павел Чернов, Петр Чернышов, Мария Федорова, Марина Светлова, Мария Савина, Мария Рыкова," +
+                " Марина Лугова, Анна Владимирова, Иван Мечников, Петр Петин, Иван Ежов";
+        System.out.println("");
+
+        text = text.replaceAll("[-|–|—]|\\p{Punct}", " ");
+        while (text.contains("  ")){
+            text = text.replace("  ", " ");
+        }
+        String[] arrayText = text.split(" ");
+
+        Map<String, Integer> map = getCountWords(arrayText);
+
+        for(Map.Entry<String, Integer> entry : map.entrySet()){
+            if(entry.getValue() > 1) {
+                System.out.println(entry.getKey() + " - " + entry.getValue());
+            }
+        }
+    }
+
+    public static Map<String, Integer> getCountWords(String[] array){
+        Map<String, Integer> map = new HashMap<>();
+
+        for (String current : array){
+            int count = map.getOrDefault(current.toLowerCase(), 0);
+            map.put(current.toLowerCase(), ++count);
+        }
+
+        return map;
+    }
+}
